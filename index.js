3f5f1821a662e2df9ca87b91d4e2dfbfc400741e
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-07-01 14:25:45 +0530

    Converted into my understanding.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const app = require("express")();
-const server = require("http").createServer(app);
+const express = require("express"); 
+const app = express();                              //Little expansion.
+const server = require("http").createServer(app);   //Used http server because need to get connected to the socket.io.
 const cors = require("cors");
 
 const io = require("socket.io")(server,{
@@ -17,6 +18,6 @@ app.get("/",(req,res) => {
     res.json("Server is running.")
 })
 
-server.listen(PORT, () => {
+server.listen(PORT, () => {             //here server is already present so no need for "app.listen()"
     console.log(`server is running on port ${PORT}`);
 })
